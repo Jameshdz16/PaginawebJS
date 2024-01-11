1d72399762f1d1c419d1a217f6e2ff612880a119
Jameshdz16 <james199508@hotmail.com>
2024-01-10 20:50:26 -0500

    realizamos una calculadora con JS

---
+++ script.js
@@ -0,0 +1,15 @@
+//agrega a la pontalla el valor del boton
+function agegar(valor){
+    document.getElementById('pantalla').value += valor 
+}
+
+function borrar(){
+    document.getElementById('pantalla').value =''
+
+}
+
+function calcular(){
+  const valorPantalla =document.getElementById('pantalla').value
+  const resultado = eval(valorPantalla)
+  document.getElementById('pantalla').value =(resultado)
+}
